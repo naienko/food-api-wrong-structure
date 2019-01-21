7313dfc2e2f6093a15de8d29b52922c39b68fd37
Panya Farnette <naienko@gmail.com>
2019-01-21 14:49:18 -0600

    some code variations

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -6,7 +6,10 @@ fetch("http://localhost:8088/foods")
                 .then(response => response.json())
                 .then(productInfo => {
                     // Use it here
-                    food.ingredients = productInfo.product.ingredients;
+                    // food.ingredients = productInfo.product.ingredients;
+                    food.ingredients = productInfo.product.ingredients.map(i => {
+                        return `${i.text}`
+                    }).join(', ');
                     food.fat = productInfo.product.nutriments.fat_serving;
                     food.fatUnit = productInfo.product.nutriments.fat_unit;
                     food.sugar = productInfo.product.nutriments.sugars_serving;
@@ -23,16 +26,16 @@ fetch("http://localhost:8088/foods")
 
 
 const foodFactory = food => {
-    const shoppingList = food.ingredients;
-    let ingredients = [];
-    for (let i = 0; i < shoppingList.length; i++) {
-        const element = shoppingList[i];
-        ingredients.push(element.text);
-    }
+    // const shoppingList = food.ingredients;
+    // let ingredients = [];
+    // for (let i = 0; i < shoppingList.length; i++) {
+    //     const element = shoppingList[i];
+    //     ingredients.push(element.text);
+    // }
     return `<section class="foodCard">
     <h2>${food.name}</h2>
     <p>${food.category}</p>
-    <p>${ingredients.join(', ')}</p>
+    <p>${food.ingredients}</p>    
     <p>${food.calories}${food.calUnit} per serving</p>
     <p>${food.sugar}${food.sugarUnit} of sugar per serving</p>
     <p>${food.fat}${food.fatUnit} of fat per serving</p>
@@ -44,3 +47,6 @@ const addFoodToDom = foodAsHTML => {
     let foodGoHere = document.querySelector(".foodList");
     foodGoHere.innerHTML += foodAsHTML;
 }
+
+
+/* <p>${ingredients.join(', ')}</p> */
