c8d68fe453853b22ea3edcf170e328d76d476522
Panya Farnette <naienko@gmail.com>
2019-01-21 11:54:01 -0600

    linked to open food facts, inserted data from there into flexboxes

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,18 +1,41 @@
 fetch("http://localhost:8088/foods")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
         parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food);
-            addFoodToDom(foodAsHTML)
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    // Use it here
+                    food.ingredients = productInfo.product.ingredients;
+                    food.fat = productInfo.product.nutriments.fat_serving;
+                    food.fatUnit = productInfo.product.nutriments.fat_unit;
+                    food.sugar = productInfo.product.nutriments.sugars_serving;
+                    food.sugarUnit = productInfo.product.nutriments.sugars_unit;
+                    food.calories = productInfo.product.nutriments.energy_serving;
+                    food.calUnit = productInfo.product.nutriments.energy_unit;
+                    console.log(food);
+
+                const foodAsHTML = foodFactory(food);
+                addFoodToDom(foodAsHTML)
+            })
         })
     })
 
 
 const foodFactory = food => {
+    const shoppingList = food.ingredients;
+    let ingredients = [];
+    for (let i = 0; i < shoppingList.length; i++) {
+        const element = shoppingList[i];
+        ingredients.push(element.text);
+    }
     return `<section class="foodCard">
     <h2>${food.name}</h2>
     <p>${food.category}</p>
+    <p>${ingredients.join(', ')}</p>
+    <p>${food.calories}${food.calUnit} per serving</p>
+    <p>${food.sugar}${food.sugarUnit} of sugar per serving</p>
+    <p>${food.fat}${food.fatUnit} of fat per serving</p>
     <p>${food.ethnicity}</p>
     </section>`;
 }
