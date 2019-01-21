a1873f53bd58026d66875ac9aa3d0a443cabf53d
Panya Farnette <naienko@gmail.com>
2019-01-21 11:08:15 -0600

    food listing in html

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -2,4 +2,22 @@ fetch("http://localhost:8088/foods")
     .then(foods => foods.json())
     .then(parsedFoods => {
         console.table(parsedFoods)
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food);
+            addFoodToDom(foodAsHTML)
+        })
     })
+
+
+const foodFactory = food => {
+    return `<section class="foodCard">
+    <h2>${food.name}</h2>
+    <p>${food.category}</p>
+    <p>${food.ethnicity}</p>
+    </section>`;
+}
+
+const addFoodToDom = foodAsHTML => {
+    let foodGoHere = document.querySelector(".foodList");
+    foodGoHere.innerHTML += foodAsHTML;
+}
